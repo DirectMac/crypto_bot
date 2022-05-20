9b2a41f895d3e1ecb1dcaa036d5b69744f57db00
Daniel Gajewski <daniel.gajewski@iteo.com>
2022-05-20 16:09:45 +0200

    :recycle: Fix formatting in prepare_data().

--- mybot/src/utils/prepare_data.py
+++ mybot/src/utils/prepare_data.py
@@ -7,13 +7,10 @@ def prepare_data(function_to_decorate):
         for item in function_to_decorate(arg):
             if arg is FormConfig.SIZE:
                 return check_size(item[arg])
-
             elif arg is FormConfig.CURRENCIES:
                 return check_currencies(item[arg])
-
             elif arg is FormConfig.PERIOD:
                 return check_period(item[arg])
-
             elif arg is FormConfig.INDEX:
                 return check_index(item[arg])
     return wrapper
